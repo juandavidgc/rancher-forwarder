992606178d778223fc8831a99cbbeef0713f3f83
Jgutierrez <juandavidgc@gmail.com>
2017-03-28 11:15:53 -0500

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # rancher-forwarder
+
+To run this image:
+
+docker run -e DST_IP="10.42.173.8" -e DST_PORT="8080" -p 93:9090 juandavidgc/rancher-forwarder
---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"net"
+	"net/http/httputil"
+	"time"
+	"os"
+)
+
+func main() {
+	http.HandleFunc("/", ReverseProxy())
+	println("ready")
+	log.Fatal(http.ListenAndServe(":9090", nil))
+}
+
+func ReverseProxy() http.HandlerFunc {
+	return func(w http.ResponseWriter, req *http.Request) {
+		transport := &http.Transport{
+			Proxy: http.ProxyFromEnvironment,
+			Dial: func(network, addr string) (net.Conn, error) {
+				return getConnection(req)
+			},
+			TLSHandshakeTimeout: 10 * time.Second,
+		}
+		(&httputil.ReverseProxy{
+			Director: func(req *http.Request) {
+				req.URL.Scheme = "http"
+				req.URL.Host = req.RequestURI
+			},
+			Transport: transport,
+		}).ServeHTTP(w, req)
+	}
+}
+
+func getConnection(req *http.Request) (net.Conn, error) {
+	return net.Dial("tcp", os.Getenv("DST_IP") + ":" + os.Getenv("DST_PORT"))
+}
